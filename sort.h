1c46d1c96ed7181652aaee8d79067e8cd58535fc
Jose Medina <fermed28@gmail.com>
2019-06-17 19:58:00 +0000

    adding print array code

--- sort.h
+++ sort.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubble_sort(int *array, size_t size)
-void insertion_sort_list(listint_t **list)
-void selection_sort(int *array, size_t size)
-void quick_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
