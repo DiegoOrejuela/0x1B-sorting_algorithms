b6ffc53e12a28799cbd562bbb47dbe3124e36a43
Jose Medina <fermed28@gmail.com>
2019-06-19 21:31:07 +0000

    solution quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,27 +2,35 @@
 
 void recursive_quicksort(int *array, int lo, int hi, size_t size)
 {
-	printf("hi  %d - lo %d\n", hi, lo);
 	int j, aux;
 	int i = lo - 1;
-	for (j = i; j < hi; j++)
+
+	for (j = lo; j < hi; j++)
 	{
-		if (array[j] <= array[hi])
+		if ((array[j] < array[hi]) )
 		{
 			i++;
 			aux = array[j];
 			array[j] = array[i];
-			array[j] = aux;
+			array[i] = aux;
+			if (i != j)
+				print_array(array, size);
 		}
 	}
-	aux = array[hi];
-	array[hi] = array[i];
-	array[i] = aux;
-	print_array(array, size);
-	printf("valor de i %d, valor de j %d- ", i, j);
-	if (i != i)
-		recursive_quicksort(array, lo, j - 1, size);
-	recursive_quicksort(array, i + 1, hi, size);
+	i++;
+	if (array[hi] != array[i])
+	{
+		aux = array[hi];
+		array[hi] = array[i];
+		array[i] = aux;
+		if (i != hi)
+			print_array(array, size);
+	}
+	if (lo < i - 1)
+		recursive_quicksort(array, lo, i - 1, size);
+	if (i + 1 < hi)
+		recursive_quicksort(array, i + 1, hi, size);
+
 }
 
 
